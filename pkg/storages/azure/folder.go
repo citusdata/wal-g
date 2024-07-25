3b050f2ea776095180a4182ad000099b1c7d7ce7
Mehmet YILMAZ <mehmet.yilmaz@microsoft.com>
2024-07-25 15:40:10 +0300

    update

--- pkg/storages/azure/folder.go
+++ pkg/storages/azure/folder.go
@@ -12,6 +12,7 @@ import (
 	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/pkg/storages/storage"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
@@ -28,7 +29,7 @@ const (
 	BufferSizeSetting = "AZURE_BUFFER_SIZE"
 	MaxBuffersSetting = "AZURE_MAX_BUFFERS"
 	TryTimeoutSetting = "AZURE_TRY_TIMEOUT"
-	ClientIDSetting   = "AZURE_CLIENT_ID"
+	MiTokenSetting    = "AZURE_STORAGE_MI_TOKEN"
 	minBufferSize     = 1024
 	defaultBufferSize = 8 * 1024 * 1024
 	minBuffers        = 1
@@ -54,7 +55,7 @@ var SettingList = []string{
 	EndpointSuffix,
 	BufferSizeSetting,
 	MaxBuffersSetting,
-	ClientIDSetting,
+	MiTokenSetting,
 }
 
 func NewFolderError(err error, format string, args ...interface{}) storage.Error {
@@ -81,21 +82,27 @@ func NewFolder(
 	}
 }
 
+// tokenCredential is a custom credential type that holds the OAuth2 token
+type tokenCredential struct {
+	token string
+}
+
+// GetToken implements the azcore.TokenCredential interface
+func (t *tokenCredential) GetToken(ctx context.Context, options policy.TokenRequestOptions) (azcore.AccessToken, error) {
+	// Assuming the token is valid for 1 hour from now
+	return azcore.AccessToken{Token: t.token, ExpiresOn: time.Now().Add(1 * time.Hour)}, nil
+}
+
 func getContainerClientWithManagedIndetity(
 	accountName string,
 	storageEndpointSuffix string,
 	containerName string,
 	timeout time.Duration,
-	clientID string) (*azblob.ContainerClient, error) {
-	cred, err := azidentity.NewManagedIdentityCredential(&azidentity.ManagedIdentityCredentialOptions{
-		ID: azidentity.ClientID(clientID),
-	})
-	if err != nil {
-		return nil, err
-	}
+	accountToken string) (*azblob.ContainerClient, error) {
+	cred := &tokenCredential{token: accountToken}
 
 	containerURLString := fmt.Sprintf("https://%s.blob.%s/%s", accountName, storageEndpointSuffix, containerName)
-	_, err = url.Parse(containerURLString)
+	_, err := url.Parse(containerURLString)
 	if err != nil {
 		return nil, NewFolderError(err, "Unable to parse service URL")
 	}
@@ -164,9 +171,9 @@ func getContainerClient(
 	return containerClient, err
 }
 
-func configureAuthType(settings map[string]string) (AzureAuthType, string, string, string) {
+func configureAuthType(settings map[string]string) (AzureAuthType, string, string) {
 	var ok bool
-	var accountToken, accessKey, clientID string
+	var accountToken, accessKey string
 	var authType AzureAuthType
 
 	if accessKey, ok = settings[AccessKeySetting]; ok {
@@ -177,11 +184,11 @@ func configureAuthType(settings map[string]string) (AzureAuthType, string, strin
 		if !strings.HasPrefix(accountToken, "?") {
 			accountToken = "?" + accountToken
 		}
-	} else if clientID, ok = settings[ClientIDSetting]; ok {
+	} else if accountToken, ok = settings[MiTokenSetting]; ok {
 		authType = AzureManagedIdentityAuth
 	}
 
-	return authType, accountToken, accessKey, clientID
+	return authType, accountToken, accessKey
 }
 
 func ConfigureFolder(prefix string, settings map[string]string) (storage.Folder, error) {
@@ -191,7 +198,7 @@ func ConfigureFolder(prefix string, settings map[string]string) (storage.Folder,
 		return nil, NewCredentialError(AccountSetting)
 	}
 
-	authType, accountToken, accountKey, clientID := configureAuthType(settings)
+	authType, accountToken, accountKey := configureAuthType(settings)
 
 	var credential *azblob.SharedKeyCredential
 	var err error
@@ -230,7 +237,7 @@ func ConfigureFolder(prefix string, settings map[string]string) (storage.Folder,
 	if authType == AzureSASTokenAuth {
 		containerClient, err = getContainerClientWithSASToken(accountName, storageEndpointSuffix, containerName, timeout, accountToken)
 	} else if authType == AzureManagedIdentityAuth {
-		containerClient, err = getContainerClientWithManagedIndetity(accountName, storageEndpointSuffix, containerName, timeout, clientID)
+		containerClient, err = getContainerClientWithManagedIndetity(accountName, storageEndpointSuffix, containerName, timeout, accountToken)
 	} else if authType == AzureAccessKeyAuth {
 		containerClient, err = getContainerClientWithAccessKey(accountName, storageEndpointSuffix, containerName, timeout, credential)
 	} else {
